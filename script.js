ca7c2da159644ab6f3f8560e53cef2750b50b2d5
davispacheco <davipacheco1996@gmail.com>
2022-01-15 17:54:38 -0300

    Implementar função traduzirCondicao, para capturar os resultados da API que estão em inglês e retornar os correspondentes em português.

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const condicao = {1: document.querySelector('#status1'), 2: document.querySelect
 const especie = {1: document.querySelector('#especie1'), 2: document.querySelector('#especie2'), 3: document.querySelector('#especie3')};
 const imagem = {1: document.querySelector('#imagem1'), 2: document.querySelector('#imagem2'), 3: document.querySelector('#imagem3')};
 const nomeDoPersonagem = {1: document.querySelector('#nome1'), 2: document.querySelector('#nome2'), 3: document.querySelector('#nome3')};
+
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos.';
+    }else if(data.status == 'Alive'){
+        return 'Sim.';
+    }else {
+        return 'Não, está morto.';
+    }
+}
