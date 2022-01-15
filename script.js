ca021509009ec05b923ee6ad1f9d9fe553256233
davispacheco <davipacheco1996@gmail.com>
2022-01-15 17:49:33 -0300

    Definir as constantes que receberão os dados.

---
+++ script.js
@@ -0,0 +1,5 @@
+const botao = document.querySelector('button');
+const condicao = {1: document.querySelector('#status1'), 2: document.querySelector('#status2'), 3: document.querySelector('#status3')};
+const especie = {1: document.querySelector('#especie1'), 2: document.querySelector('#especie2'), 3: document.querySelector('#especie3')};
+const imagem = {1: document.querySelector('#imagem1'), 2: document.querySelector('#imagem2'), 3: document.querySelector('#imagem3')};
+const nomeDoPersonagem = {1: document.querySelector('#nome1'), 2: document.querySelector('#nome2'), 3: document.querySelector('#nome3')};
